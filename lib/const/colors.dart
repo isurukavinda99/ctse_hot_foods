0a519d83f1919dee0eb937ecce037e881168a2c7
it19164236 <it19164236@my.sliit.lk>
2023-03-24 15:09:56 +0530

    Colors.dart file updated

---
+++ lib/const/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const base = Color.fromARGB(255, 2, 48, 32);
+  static const primary = Color(0xFF4A4B4D);
+  static const secondary = Color(0xFF7C7D7E);
+  static const placeholder = Color(0xFFB6B7B7);
+  static const placeholderBg = Color(0xFFF2F2F2);
+}
